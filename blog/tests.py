1b5e29f0ce3367d6cac845be5ae35923621845e3
sandalu umayanga <sandaluumayangaofficial@gmail.com>
2025-01-31 19:59:39 +0530

    new tests

--- blog/tests.py
+++ blog/tests.py
@@ -1,7 +1,5 @@
-
 from django.test import TestCase
-from . models import Post
-
+from .models import Post
 
 class ModelTesting(TestCase):
 
@@ -12,3 +10,23 @@ class ModelTesting(TestCase):
         d = self.blog
         self.assertTrue(isinstance(d, Post))
         self.assertEqual(str(d), 'django')
+
+    def test_addition(self):
+        self.assertEqual(1 + 1, 2)
+        self.assertEqual(2 + 2, 4)
+        self.assertEqual(3 + 3, 6)
+
+    def test_subtraction(self):
+        self.assertEqual(5 - 3, 2)
+        self.assertEqual(10 - 5, 5)
+        self.assertEqual(20 - 10, 10)
+
+    def test_multiplication(self):
+        self.assertEqual(2 * 3, 6)
+        self.assertEqual(4 * 5, 20)
+        self.assertEqual(6 * 7, 42)
+
+    def test_division(self):
+        self.assertEqual(10 / 2, 5)
+        self.assertEqual(20 / 4, 5)
+        self.assertEqual(9 / 3, 3)
